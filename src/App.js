d4190985b5c82409c2e31d5d0002d7d8ceea308c
Arpit Sheth <shetharp@gmail.com>
2017-07-14 02:47:34 -0400

    Sets up the website. Adds SASS to workflow.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+import Home from './Home.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <h1>Arpit Sheth</h1>
+        <h2>Home</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
