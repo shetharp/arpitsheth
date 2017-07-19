6161a7d4eacbb2689ac19925e5bb32af040dd7da
Arpit Sheth <shetharp@gmail.com>
2017-07-19 00:30:17 -0400

    Adds photos to home page slides

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -10,14 +10,14 @@ class Home extends Component {
 
         <Slide id="intro">
           <SlideTitle>
-            <strong>Hey, I'm Arpit.</strong>
+            <strong>Hey, I’m Arpit.</strong>
           </SlideTitle>
           <SlideBody><em className="Slide__body_font-inf">Tech</em> pro with <em className="Slide__body_font-inf">creative</em> tendencies and a knack for <em className="Slide__body_font-inf">business</em>.</SlideBody>
         </Slide>
 
         <Slide id="cornell-tech" wide={true} button="Contact Me" link="contact">
           <SlideTitle>
-            Currently, I'm getting my MEng in <em>computer science</em> at <strong>Cornell Tech</strong>.
+            Currently, I’m getting my MEng in <em>computer science</em> at <strong>Cornell Tech</strong>.
           </SlideTitle>
           <SlideBody>
             <strong>Cornell Tech MEng &rsquo;18</strong><br />
@@ -31,6 +31,65 @@ class Home extends Component {
           </SlideTitle>
           <SlideBody><strong>Featured:</strong> Cornell Mars Rover</SlideBody>
         </Slide>
+
+        <Slide id="public-speaking" wide={true} button="Learn More" link="/">
+          <SlideTitle>
+            I <strong>speak</strong> to inspire interest in <em>STEM, design, and leadership</em>.
+          </SlideTitle>
+          <SlideBody><strong>Featured:</strong> USA Science & Engineering Festival</SlideBody>
+        </Slide>
+
+        <Slide id="entrepreneurship" button="Learn More" link="/">
+          <SlideTitle>
+            I <strong>mentor</strong> students with <em>entrepreneurial ambitions</em>.
+          </SlideTitle>
+          <SlideBody><strong>Featured:</strong> 3 Day Startup at Cornell</SlideBody>
+        </Slide>
+
+        <Slide id="data-design" wide={true} button="View Case Study" link="/">
+          <SlideTitle>
+            I combine <strong>data & design</strong> to make <em>information accessible</em>.
+          </SlideTitle>
+          <SlideBody><strong>Featured:</strong> Cornell University Emergency Medical Service</SlideBody>
+        </Slide>
+
+        <Slide id="cloud-computing" wide={true} button="View Project" link="/">
+          <SlideTitle>
+            I’ve researched how to <em>detect cancer</em> with <strong>cloud computing</strong>.
+          </SlideTitle>
+          <SlideBody><strong>Featured:</strong> NJ Governor’s School of Engineering and Technology</SlideBody>
+        </Slide>
+
+        <Slide id="artificial-creativity" wide={true} button="View Project" link="/">
+          <SlideTitle>
+            I beleive <strong>artificial creativity</strong> will empower the future of <em>expression and innovation</em>.
+          </SlideTitle>
+          <SlideBody><strong>Featured:</strong> Origami Fill</SlideBody>
+        </Slide>
+
+        <Slide id="art" button="View Project" link="/">
+          <SlideTitle>
+            Sometimes, I burn the <em>midnight oil</em> working on <strong>art</strong>.
+          </SlideTitle>
+          <SlideBody><strong>Featured:</strong> A Space Journey</SlideBody>
+        </Slide>
+
+        <Slide id="photos" button="View Photos" link="//instagram.com/shetharp" linkExternal={true}>
+          <SlideTitle>
+            I <em>explore the world</em> around me through <strong>photography</strong>.
+          </SlideTitle>
+          <SlideBody><strong>Featured:</strong> Instagram Profile</SlideBody>
+        </Slide>
+
+        <Slide id="more" wide={true} button="View Photos" link="/">
+          <SlideTitle>
+            There’s more <strong>awesome</strong> to <em>discover</em>!
+          </SlideTitle>
+          <SlideBody>
+            <strong>Blog</strong> • <strong>Instagram</strong> • <strong>Contact</strong>
+          </SlideBody>
+        </Slide>
+
       </div>
     );
   }
