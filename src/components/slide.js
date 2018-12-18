1716ebc083e82c59a9fe04b12ffbb962b1e460f9
Arpit Sheth <shetharp@gmail.com>
2018-12-18 01:11:31 -0500

    Adds slides and implements styles for mobile

--- src/components/slide.js
+++ src/components/slide.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { rhythm, scale } from '../utils/typography'
 import styled from 'styled-components'
 import { rgba } from 'polished'
 import { Link } from 'gatsby'
@@ -14,8 +15,8 @@ const StyledSlide = styled.section`
   width: 100%;
   height: 100vh;
   position: relative;
-  border: ${props => (props.noBorder ? 'none' : '16px solid white')};
-  margin-bottom: ${props => (props.noBorder ? '0' : '-16px')};
+  border: ${props => (props.isBorderless ? 'none' : '16px solid white')};
+  margin-bottom: ${props => (props.isBorderless ? '0' : '-16px')};
 `
 const Overlay = styled.div`
   position: absolute;
@@ -24,7 +25,7 @@ const Overlay = styled.div`
   z-index: -1;
   width: 100%;
   height: 100%;
-  background: ${props => rgba(props.overlayBg, 0.4)};
+  background: ${props => rgba(props.overlay, 0.5)};
   /* background: linear-gradient(
     175deg,
     rgba(0, 0, 0, 0) 0%,
@@ -49,20 +50,29 @@ const Content = styled.div`
   /* border: 4px solid springgreen; */
 `
 const Body = styled.div`
-    /* outline: 4px dotted palevioletred; */
+  /* outline: 4px dotted palevioletred; */
 `
 const Title = styled.h1`
   font-size: 1.375rem;
   font-weight: normal;
   /* outline: 1px dotted palevioletred; */
 `
-const Description = styled.p`
-  /* font-style: italic; */
+const Description = styled.div`
   line-height: 1.375;
+  a {
+    transition: background-color 150ms ease;
+  }
+  a:hover {
+    background: ${props => rgba(props.overlay, 0.4)};
+  }
+  em {
+    font-style: normal;
+    background: ${props => rgba(props.overlay, 0.4)};
+  }
 `
 
 /* ==================================================
- *  Helper Components
+ *  Helpers
 ================================================== */
 /**
  * Creates a button with some text and a link location.
@@ -85,18 +95,18 @@ function Button(props) {
 }
 
 /* ==================================================
- *  Render Component
+ *  Render
 ================================================== */
 /**
  * Creates a Slide which is a fullscreen section (height: 100vh; width: 100%)
  * with a background image, title, and body text.
  */
 function Slide(props) {
-  console.log(props)
   return (
     <StyledSlide {...props}>
       <Img
         fluid={props.fluid}
+        alt={props.id}
         // This CSS allows the image to be full screen.
         // https://github.com/gatsbyjs/gatsby/issues/2470#issuecomment-338394370
         css={`
@@ -114,11 +124,11 @@ function Slide(props) {
           }
         `}
       />
-      <Overlay overlayBg={props.overlay}/>
+      <Overlay overlay={props.overlay} />
       <Content>
         <Body>
           <Title>{props.title}</Title>
-          <Description>{props.children}</Description>
+          <Description overlay={props.overlay}>{props.descr}</Description>
         </Body>
 
         {/* If this Slide has a button property, render a button */
@@ -149,17 +159,21 @@ function Slide(props) {
 }
 
 /* ==================================================
- *  Component Properties
+ *  Properties
 ================================================== */
 Slide.propTypes = {
-  // The Slide Title
+  // The ID used in the HTML prop for linking
+  id: PropTypes.string.isRequired,
+  // The title text
   title: PropTypes.string,
+  // The description wrapped in a React/HTML element, preferably <p>
+  descr: PropTypes.element,
   // The fluid image data for gatsby-image
   fluid: PropTypes.object,
-  // The position of the image. Centered by default
-  position: PropTypes.string,
-  // The Overlay color as #HEX or "none"
+  // The Overlay color in #000fff format
   overlay: PropTypes.string,
+  // The position of the image in CSS object-position format
+  position: PropTypes.string,
   // A Button with text, link location, and indicator if the link is external
   button: PropTypes.shape({
     text: PropTypes.string.isRequired,
@@ -167,13 +181,14 @@ Slide.propTypes = {
     isExternal: PropTypes.bool.isRequired,
   }),
   // Displays a slide without a border
-  noBorder: PropTypes.bool,
+  isBorderless: PropTypes.bool,
 }
 
 Slide.defaultProps = {
   position: '50% 50%',
   overlay: '#000000',
-  noBorder: false,
+  overlayImg: `''`,
+  isBorderless: false,
 }
 
 /* ==================================================
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,75 +12,248 @@ import SEO from '../components/seo'
 // ...
 
 /* ==================================================
- *  Render Component
+ *  Render
 ================================================== */
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO
-      title="Home"
-      keywords={['arpit', 'sheth', 'shetharp', 'cornell', 'tech']}
-    />
+function IndexPage({ data }) {
+  /* --------------------------------------------------
+   *  Slide Data
+  -------------------------------------------------- */
+  // slides contains the props data needed to create <Slide> elements.
+  // For each slide that we want to render, we create an object and push it to slides.
+  const slides = []
 
-    {/* ==================================================
-        *   Slide / Intro
-      ================================================== */}
-    <Slide
-      id="intro"
-      title="I&rsquo;m an interdisciplinary engineer with creator instincts and a knack for starting things."
-      fluid={data.imgIntro.childImageSharp.fluid}
-      position="50% 0%"
-      button={{ text: 'Learn More', link: '#', isExternal: false }}
-      noBorder
-    >
-      I co-founded <Link to="#">Crater</Link> and recently graduated from{' '}
-      <Link to="#">Cornell Tech</Link> with an M.Eng in CS. I'm now looking for
-      exciting opportunities.
-    </Slide>
+  /* --------------------------------------------------
+   *  Slide Data / Intro
+  -------------------------------------------------- */
+  slides.push({
+    id: 'intro',
+    title:
+      'I’m an interdisciplinary engineer with creator instincts and a knack for starting things.',
+    descr: (
+      <p>
+        I co-founded <Link to="#crater">Crater</Link> and was early at{' '}
+        <Link to="#matter">Matter</Link>. I recently graduated from{' '}
+        <Link to="#cornell">Cornell Tech</Link> with an M.Eng in CS and am
+        looking for exciting new opportunities.
+      </p>
+    ),
+    fluid: data.imgIntro.childImageSharp.fluid,
+    // overlay: '#0000ff',
+    position: '50% 0%',
+    // button: { text: 'Learn More', link: '#', isExternal: false },
+    isBorderless: true,
+  })
 
-    {/* ==================================================
-        *   Slide / Crater
-      ================================================== */}
-    <Slide
-      id="crater"
-      title="I was Co-founder & CEO at Crater, a synthetic media startup building local TV for the next generation."
-      fluid={data.imgCrater.childImageSharp.fluid}
-      button={{
-        text: 'Visit Crater',
-        link: 'https://crater.land',
-        isExternal: true,
-      }}
-    >
-      I led a highly creative and deeply technical team of engineers, designers,
-      and product managers. We built an AI-powered video production app for iOS
-      & Android and raised $100,000 in VC funding.
-    </Slide>
+  /* --------------------------------------------------
+   *  Slide Data / Crater
+  -------------------------------------------------- */
+  slides.push({
+    id: 'crater',
+    title:
+      'I was Co-founder & CEO at Crater, a synthetic media startup building local TV for the next generation.',
+    descr: (
+      <p>
+        I led a highly creative and deeply technical team of engineers,
+        designers, and product managers. We built an AI-powered video production
+        app, got funding, and were featured as an NYC startup to watch.
+      </p>
+    ),
+    fluid: data.imgCrater.childImageSharp.fluid,
+    // overlay: '#0000ff',
+    position: '75% 50%',
+    button: {
+      text: 'Learn More',
+      link:
+        'https://www.technyc.org/posts/2018/9/five-cornell-tech-companies-to-watch',
+      isExternal: true,
+    },
+    // isBorderless: false,
+  })
 
-    {/* ==================================================
-        *   Slide / Cornell
-      ================================================== */}
-    <Slide
-      id="cornell"
-      title="I recently graduated with an M.Eng in Computer Science from Cornell Tech."
-      fluid={data.imgCornell.childImageSharp.fluid}
-    >
-      In the Studio, I worked with The Bill & Melinda Gates Foundation to{' '}
-      <a href="https://tech.cornell.edu/news/cornell-tech-students-lead-the-way-in-building-technology-with-real-life-im/">
-        provide digital financial services
-      </a>{' '}
-      for the next billion people.
-      I won an award for designing a civic tech
-      solution to <a href="https://tech.cornell.edu/news/remaking-the-city-masters-students-build-products-for-roosevelt-island-comm/">
-        promote urban greenspaces.
-      </a>
-      <br />
-      <br />I also studied CS in undergrad at Cornell University.
-    </Slide>
-  </Layout>
-)
+  /* --------------------------------------------------
+   *  Slide Data / Cornell
+  -------------------------------------------------- */
+  slides.push({
+    id: 'cornell',
+    title:
+      'I recently graduated with an M.Eng in Computer Science from Cornell Tech.',
+    descr: (
+      <>
+        <p>
+          In the Studio, I worked with The Bill & Melinda Gates Foundation to
+          provide{' '}
+          <a href="https://tech.cornell.edu/news/cornell-tech-students-lead-the-way-in-building-technology-with-real-life-im/">
+            digital financial services
+          </a>{' '}
+          to the next billion people. I also won an award for designing a civic
+          tech solution to{' '}
+          <a href="https://tech.cornell.edu/news/remaking-the-city-masters-students-build-products-for-roosevelt-island-comm/">
+            promote urban greenspaces.
+          </a>
+        </p>
+        <p>
+          <em>I studied CS in undergrad at Cornell University.</em>
+        </p>
+      </>
+    ),
+    fluid: data.imgCornell.childImageSharp.fluid,
+    overlay: '#0000ff',
+    position: '0% 50%',
+    // button: { text: 'Learn More', link: '#', isExternal: true,},
+    // isBorderless: false,
+  })
+
+  /* --------------------------------------------------
+   *  Slide Data / Matter
+  -------------------------------------------------- */
+  slides.push({
+    id: 'matter',
+    title:
+      'At Matter, I built products to bring the future of manufacturing to independent designers, artisans, and fabricators.',
+    descr: (
+      <p>
+        I was awarded a fully sponsored fellowship by the{' '}
+        <a href="https://www.engineering.cornell.edu/students/undergraduate-students/entrepreneurial-options-undergrad-students/kessler-fellows-program">
+          Kessler Fellows Program
+        </a>{' '}
+        and chose to work directly with the co-founders of Matter, an
+        early-stage startup backed by Techstars. I researched over 3,000 machine
+        shops and shipped products to help steer the company towards{' '}
+        <a href="https://angel.co/matter-io">strategic acquisition.</a>
+      </p>
+    ),
+    fluid: data.imgMatter.childImageSharp.fluid,
+    // overlay: '#0000ff',
+    position: '0% 50%',
+    // button: { text: 'Learn More', link: '#', isExternal: true,},
+    // isBorderless: false,
+  })
+
+  /* --------------------------------------------------
+   *  Slide Data / Mentoring
+  -------------------------------------------------- */
+  slides.push({
+    id: 'mentoring',
+    title: 'I mentor students with entrepreneurial ambitions.',
+    descr: (
+      <>
+        <p>
+          Along with my friends, I launched{' '}
+          <a href="http://cornell.3daystartup.org/">3 Day Startup at Cornell</a>{' '}
+          to teach tech entrepreneurship. I also advised and did pro bono design
+          work for portfolio companies at{' '}
+          <a href="http://www.lifechanginglabs.com/">Life Changing Labs</a>.
+        </p>
+        <p>
+          <em>
+            If you’re working on something neat, I'm happy to help however I
+            can.
+          </em>
+        </p>
+      </>
+    ),
+    fluid: data.imgMentoring.childImageSharp.fluid,
+    // overlay: '#0000ff',
+    position: '20% 50%',
+    button: { text: 'Let’s Chat', link: '#', isExternal: false },
+    // isBorderless: false,
+  })
+
+  /* --------------------------------------------------
+   *  Slide Data / Speaking
+  -------------------------------------------------- */
+  slides.push({
+    id: 'speaking',
+    title: 'I speak to inspire career & creativity in tech.',
+    descr: (
+      <p>
+        I got my certification at the Cornell <a href="https://www.engineering.cornell.edu/students/undergraduate-students/special-programs/engineering-leadership-programs/engineering">Engineering Leadership Program</a> and
+        went on to become President of <a href="http://cornellthetatau.com/">Cornell Theta Tau</a> to promote career
+        leadership for engineers on campus. I've spoken at the <a href="https://usasciencefestival.org/">USA Science
+        & Engineering Festival</a> and NJ Governor's School.
+      </p>
+    ),
+    fluid: data.imgSpeaking.childImageSharp.fluid,
+    // overlay: '#0000ff',
+    position: '60% 50%',
+    // button: { text: 'Learn More', link: '#', isExternal: false },
+    // isBorderless: false,
+  })
+
+  /* --------------------------------------------------
+   *  Slide Data / Design
+  -------------------------------------------------- */
+  slides.push({
+    id: 'design',
+    title: 'I design branding to help organizations embody their culture and values.',
+    descr: (
+      <>
+        <p>
+          The branding initiative I launched at <a href="http://marsrover.engineering.cornell.edu/">Cornell Mars Rover</a> helped catapult the team from underdogs to one of the most competitive.
+      </p>
+        <p><em>For over ten years I've been teaching myself design. It is core to how I build products and lead teams.</em></p>
+      </>
+    ),
+    fluid: data.imgDesign.childImageSharp.fluid,
+    // overlay: '#0000ff',
+    // position: '50% 50%',
+    // button: { text: 'Learn More', link: '#', isExternal: false },
+    // isBorderless: false,
+  })
+
+  /* --------------------------------------------------
+   *  Slide Data / Creative
+  -------------------------------------------------- */
+  slides.push({
+    id: 'creative',
+    title: `AI may come after our creativity, but it can't take away our expression.`,
+    descr: (
+      <p>
+        I make time for art. Sometimes, I draw{' '}
+        <a href="https://drive.google.com/drive/folders/1zr4VldrOsgcPi33BazRQfmpSDjg0ZGp4?usp=sharing">
+          sci-fi illustrations.
+        </a>{' '}
+        But mostly, I photograph the world to savor its subtle splendor.
+      </p>
+    ),
+    fluid: data.imgCreative.childImageSharp.fluid,
+    // overlay: '#0000ff',
+    position: '52.5% 50%',
+    button: {
+      text: 'View Photos',
+      link: 'https://www.instagram.com/shetharp/',
+      isExternal: true,
+    },
+    // isBorderless: false,
+  })
+
+  /* --------------------------------------------------
+   *  Slide Data / Render
+  -------------------------------------------------- */
+  // Why map is used: https://reactjs.org/docs/lists-and-keys.html
+  // Why a 'key' prop is needed: https://reactjs.org/docs/lists-and-keys.html#keys
+  const renderSlides = slides.map(slideProps => (
+    <Slide key={slideProps.id} {...slideProps} />
+  ))
+
+  /* --------------------------------------------------
+  *  Render Component
+  -------------------------------------------------- */
+  return (
+    <Layout>
+      <SEO
+        title="Home"
+        keywords={['arpit', 'sheth', 'shetharp', 'cornell', 'tech']}
+      />
+
+      {renderSlides}
+    </Layout>
+  )
+}
 export default IndexPage
 
 /* ==================================================
- *  Component Properties
+ *  Properties
 ================================================== */
 // ...
 
@@ -102,13 +275,30 @@ export const slideImage = graphql`
 // See https://codebushi.com/using-gatsby-image/ for more info
 export const pageQuery = graphql`
   query {
-    imgIntro: file(relativePath: { eq: "slides/intro.jpg" }) {
+    imgIntro: file(relativePath: { eq: "slides/intro-arpit-sheth.jpg" }) {
       ...slideImage
     }
-    imgCrater: file(relativePath: { eq: "slides/crater.jpg" }) {
+    imgCrater: file(relativePath: { eq: "slides/crater-land-team.jpg" }) {
       ...slideImage
     }
-    imgCornell: file(relativePath: { eq: "slides/cornell.jpg" }) {
+    imgCornell: file(relativePath: { eq: "slides/cornell-tech.jpg" }) {
+      ...slideImage
+    }
+    imgMatter: file(relativePath: { eq: "slides/matter.jpg" }) {
+      ...slideImage
+    }
+    imgMentoring: file(
+      relativePath: { eq: "slides/mentoring-student-startups.jpg" }
+    ) {
+      ...slideImage
+    }
+    imgSpeaking: file(relativePath: { eq: "slides/speaking-arpit-sheth.jpg" }) {
+      ...slideImage
+    }
+    imgDesign: file(relativePath: { eq: "slides/design-cornell-mars-rover.jpg" }) {
+      ...slideImage
+    }
+    imgCreative: file(relativePath: { eq: "slides/creative-udaipur-palace.jpg" }) {
       ...slideImage
     }
   }
