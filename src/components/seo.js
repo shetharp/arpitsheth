dff6dab8d604d6525056bef7887fed5f6649f1ce
Arpit Sheth <shetharp@gmail.com>
2018-12-26 15:58:23 -0500

    Adds an OpenGraph Image

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
+import openGraphImage from '../images/arpit-sheth-og.png';
 
 function SEO({ description, lang, meta, keywords, title }) {
   return (
@@ -28,6 +29,10 @@ function SEO({ description, lang, meta, keywords, title }) {
               {
                 property: 'og:description',
                 content: metaDescription,
+              },
+              {
+                property: 'og:image',
+                content: openGraphImage,
               },
               {
                 property: 'og:type',
